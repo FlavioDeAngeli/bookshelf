c4a9305b66c5add2e7bec09c0b56681bfd9a44d8
FlavioDeAngeli <flaviodeangeli@gmail.com>
2023-03-12 16:10:03 +0100

    database connection and first selections views

--- views/index.view.php
+++ views/index.view.php
@@ -1,11 +1,41 @@
 <?php
 require("layout/head.php");
 require("layout/navbar.php");
+
+class Database {
+    public function query()
+    {
+        $dsn = "mysql:host=localhost;port=3306;dbname=bookshelf;user=root;charset=utf8mb4";
+
+        $pdo = new PDO($dsn);
+
+        $statement = $pdo->prepare("SELECT books.title, authors.name, authors.surname, books.year, categories.category FROM books JOIN authors ON books.author_id = authors.id JOIN categories ON books.category_id = categories.id");
+
+        $statement->execute();
+
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+$db = new Database();
+$books = $db->query();
 ?>
 <main>
-        <div class="container-fluid main-content text-center mt-5">
-        <h1>Index Page - Show User Books</h1>
-        </div>
-    </main>
+    <div class="container-fluid ps-4 mt-3">
+        <h2>Lista dei miei libri:</h2>
+    </div>
+    <div class="list-group ps-2 pe-2 mt-3">
+        <?php foreach ($books as $book) {
+            echo "<a href='#' class='list-group-item list-group-item-action'>"
+            . $book['title'] ." - " . $book['name'] . " " . $book['surname'] ." - " . $book['year'] ." - " . $book['category'] . "</a>";
+        } ?>
+    </div>
+</main>
 
 <?php require("layout/footer.php"); ?> 
+
+<!--TEST QUERY:
+
+SELECT books.title, authors.name, authors.surname, books.year, categories.category FROM books JOIN authors ON books.author_id = authors.id JOIN categories ON books.category_id = categories.id;
+
+INSERT INTO books (title, author_id, year, category_id) VALUES ('Io sono vivo, voi siete morti', '2', '1993', '4');-->
